109c4e0631b4d31e271f7c62d2e5f99b99e52e43
Max Metral <max@pyralis.com>
2017-03-28 22:13:19 -0400

    New json schema, read db accordingly

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import path from 'path';
 import WebSocket from 'ws';
 import pgp from 'pg-promise';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 3000;
 const PGURL = process.env.DATABASE_URL || 'postgres://docker:postgis@localhost:5432/moodring'
 
 const db = pgp({})(PGURL);
 
-const server = express()
+const app = express();
+const server = app
   .use(express.static(path.join(__dirname, '..', 'static')))
+  .use(bodyParser.json())
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.post('/update', async (req, res) => {
+  await db.result(`INSERT INTO instruction (user_id, instruction)
+    SELECT user_id, $1 FROM "user" WHERE identifier = $2
+    ON CONFLICT (user_id) DO UPDATE SET instruction = $1`, [
+      req.body.instruction,
+      req.body.id,
+    ]);
+    res.sendStatus(200);
+});
 
 const wss = new WebSocket.Server({ server });
 
@@ -27,10 +40,10 @@ wss.on('connection', function connection(ws) {
     user JOIN instruction ON user.user_id = instruction.user_id`)
     .then((users) => {
       const parts = [];
-      user.forEach((i) => {
+      users.forEach((i) => {
         parts.push(user.pixel_id);
         const bits = [];
-        user.instruction.forEach((i) => {
+        user.instruction.flow.forEach((i) => {
           bits.push(...i.color, i.ticks);
         });
         parts.push(bits.join(','));
